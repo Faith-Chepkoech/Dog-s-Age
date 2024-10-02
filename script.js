f2c19cdf9ebb26b1e214cc433af64738b8cd4ff0
unknown <fchepkoech76@gmail.com>
2024-10-02 11:16:56 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+//my age
+let myAge = 23;
+//variable will change
+var earlyYears = 2;
+var earlyYears = earlyYears * 10.5;
+//first two years already accounted for
+var laterYears = myAge - 2;
+//assign in one step
+var dog = laterYears * 4;
+console.log (earlyYears);
+console.log(laterYears);
+//my age in dog years
+var myAgeInDogYears = earlyYears + laterYears;
+//builtin method
+var myName = "Faith".toLowerCase();
+console.log("My name is" + myName +"." +"I am" + myAge + "years old in human years which is " +myAgeInDogYears +"years old in dog years.");
+
+
